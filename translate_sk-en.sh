a768ec16b96fea93e94318be2308cb54e5d4012e
Fabio Bucher <fabio.bucher@uzh.ch>
2025-10-06 18:07:13 +0200

    added data and bash script for translation from slovak to english

--- translate_sk-en.sh
+++ translate_sk-en.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
-#SBATCH --output=translate.out
+#SBATCH --output=translate_sk-en.out
 
 module load gpu
 module load mamba
@@ -16,9 +16,9 @@ export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/pkgs/cuda-toolkit
 # TRANSLATE
 python translate.py \
     --cuda \
-    --input ~/shares/cz-en/data/raw/test.cz \
+    --input data_sk-en/DGT.en-sk_trim.sk \
     --src-tokenizer cz-en/tokenizers/cs-bpe-8000.model \
     --tgt-tokenizer cz-en/tokenizers/en-bpe-8000.model \
     --checkpoint-path cz-en/checkpoints/checkpoint_best.pt \
-    --output cz-en/output.txt \
+    --output sk-en_output.txt \
     --max-len 300
